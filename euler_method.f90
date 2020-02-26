93d9173dbe9a561d9104e2b71b26044061089bb5
LukMarks <lucas.mmoreno@hotmail.com>
2020-02-26 20:50:10 -0300

    Add ODE function

--- euler_method.f90
+++ euler_method.f90
@@ -4,8 +4,23 @@
 ! to solve ODE with Euler's
 ! method
 program euler
+implicit none
+
+real,external::f ! add the function in the main program
+real :: x,y,h !declaring variables
+integer ::n
+print *, "Insert the initial condtions for x"
+read*, x
+print *, "Insert the initial condtions for y"
+read *,y
+print *, "Insert the initial interval for x"
+read *,h
+
+
+
 end program euler
 
 real function f(x,y)
+real :: x,y
 f = x+2*y
 end
