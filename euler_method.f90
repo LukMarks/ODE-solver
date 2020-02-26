2db3f6535c14894265ebd2f6e8c87f7349225c20
LukMarks <lucas.mmoreno@hotmail.com>
2020-02-26 20:43:39 -0300

    Add the initial file

---
+++ euler_method.f90
@@ -0,0 +1,11 @@
+! Author: Lucas Marques Moreno
+! Date: 26/02/20
+! Description: An exercise 
+! to solve ODE with Euler's
+! method
+program euler
+end program euler
+
+real function f(x,y)
+f = x+2*y
+end
