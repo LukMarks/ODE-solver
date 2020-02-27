0b372ee7ecba4fe7991b779771054143d8ecb5fe
LukMarks <lucas.mmoreno@hotmail.com>
2020-02-26 21:09:53 -0300

    Finish method and compile

--- euler_method.f90
+++ euler_method.f90
@@ -7,20 +7,30 @@ program euler
 implicit none
 
 real,external::f ! add the function in the main program
-real :: x,y,h !declaring variables
-integer ::n
+real :: x,y,h,xg !declaring variables
+integer ::n,i
 print *, "Insert the initial condtions for x"
 read*, x
 print *, "Insert the initial condtions for y"
 read *,y
-print *, "Insert the initial interval for x"
+print *, "Insert the initial interval step for x"
 read *,h
+print *, "Insert the x soution point"
+read *,xg
 
 
+n=int(ceiling((xg-x)/h))
+do i=1,n
+    x=x+h
+    y=y+h*f(x,y)
+    print*,"Value of x: ",x,"Value of y: ",y
+end do
+
+print*,"Value of x: ",x,"Value of y: ",y
 
 end program euler
 
 real function f(x,y)
-real :: x,y
-f = x+2*y
+real :: x,y ! define variables
+f = x+2*y ! here is the same as y'
 end
