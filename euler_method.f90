8bfb318835546296eb958f1f2ad6dbe9e5d2f5c6
LukMarks <lucas.mmoreno@hotmail.com>
2020-02-28 05:23:00 -0300

    change main program for subroutine

--- euler_method.f90
+++ euler_method.f90
@@ -3,34 +3,27 @@
 ! Description: An exercise 
 ! to solve ODE with Euler's
 ! method
-program euler
-implicit none
 
-real,external::f ! add the function in the main program
-real :: x,y,h,xg !declaring variables
-integer ::n,i
-print *, "Insert the initial condtions for x"
-read*, x
-print *, "Insert the initial condtions for y"
-read *,y
-print *, "Insert the initial interval step for x"
-read *,h
-print *, "Insert the x soution point"
-read *,xg
+subroutine euler (y,f,x0,y0,h,xg)
 
-
-n=int(ceiling((xg-x)/h))
-do i=1,n
-    x=x+h
-    y=y+h*f(x,y)
-    print*,"Value of x: ",x,"Value of y: ",y
-end do
-
-print*,"Value of x: ",x,"Value of y: ",y
-
-end program euler
-
-real function f(x,y)
-real :: x,y ! define variables
-f = x+2*y ! here is the same as y'
-end
+    implicit none
+    
+    real, external :: f ! add the function in the main program
+    
+    real,intent(in) :: x0,y0,h,xg !declaring variables
+    
+    real,intent(out) :: y
+    
+    real :: x
+    integer ::n,i
+    
+    x = x0
+    y = y0
+    n=int(ceiling((xg-x)/h))
+    do i=1,n
+        x=x+h
+        y=y+h*f(x,y)
+    end do
+    
+    return
+    end subroutine euler
