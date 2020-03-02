1f3931fe3eb36aecd59c7f91626b8358c6d32d39
LukMarks <lucas.mmoreno@hotmail.com>
2020-03-02 20:56:07 -0300

    Update the exemple file

--- exemple.py
+++ exemple.py
@@ -4,3 +4,19 @@
 # about how to use my ode solver
 
 import ode
+import callback as cb
+
+async def f(x,y_l):
+    f = x+2*y_l
+    return
+
+g = lambda x, y_l : x+2*y_l 
+
+x0 = 0. # initial value for x
+y0_l = 0. # initial value for y'
+h =1e-6 # steps for the solution iterations
+x = tuple([1.]) # solution the ODE at that point in x 
+y = 0 # initial value for the solution
+#y_euler = ode.euler(x0,f,x0,y0_l,h,x)
+y_rk4 = ode.rk4(y,f,x0,y0_l,h,x)
+print(y_rk4)
